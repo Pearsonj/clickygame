fb09007359d6c6fe2ffb428785820cb5cd24ad9d
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-01-07 15:36:04 -0800

    Updates

---
+++ precache-manifest.3147ec7ff6ca51477aba52b0e4ba6db0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "25a9c1c606377ce338fe",
+    "url": "/clickygame/static/js/main.25a9c1c6.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "25a9c1c606377ce338fe",
+    "url": "/clickygame/static/css/main.1a85e46f.chunk.css"
+  },
+  {
+    "revision": "b674697966b655e51506a20dc3e1cac3",
+    "url": "/clickygame/index.html"
+  }
+];
